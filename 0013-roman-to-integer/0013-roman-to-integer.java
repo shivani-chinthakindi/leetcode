b8e2c002364a3be0079373ae837a400cc1cf1bda
Shivani Patel <schinthakindishivani@gmail.com>
2026-08-19 22:29:27 +0530

    Time: 2 ms (100%), Space: 46.4 MB (62.94%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,33 @@
+class Solution {
+    public static int romanToInt(String s) {
+      int res = 0, num = 0, prev = 0;
+      for(int i = s.length()-1; i >= 0; i--){
+        switch(s.charAt(i)){
+            case 'I': num = 1;
+                break;
+            case 'V': num = 5;
+                break;
+            case 'X': num = 10;
+                break;
+            case 'L': num = 50;
+                break;
+            case 'C': num = 100;
+                break;
+            case 'D': num = 500;
+                break;
+            case 'M': num = 1000;
+                break;
+        }
+        if(num < prev)
+         res -= num;
+        else
+         res += num;
+        prev = num;
+      }
+      return res;
+    }
+    public static void main(String[] args){
+        String s = "III";
+        System.out.println(romanToInt(s));
+    }
+}
